92cb13dafbee08e218460e5f33f7b30cbc6f224b
Dima Usov <dmitry.usov@yahoo.com>
2024-10-07 00:29:46 +0400

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,42 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = 'files';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const FILES_COPY_DIR = 'files_copy';
+const FILES_PATH = path.resolve(__dirname, FILES_DIR);
+
+const FILES_COPY_PATH = path.resolve(__dirname, FILES_COPY_DIR);
+
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access(FILES_PATH);
+
+		await fs.mkdir(FILES_COPY_PATH);
+
+		const dirEntries = await fs.readdir(FILES_PATH, { withFileTypes: true });
+
+    const copyPromises = dirEntries
+      .filter((entry) => entry.isFile())
+      .map((file) =>
+        fs.copyFile(
+          path.resolve(__dirname, FILES_DIR, file.name),
+          path.resolve(__dirname, FILES_COPY_DIR, file.name),
+          fs.constants.COPYFILE_EXCL
+        )
+      );
+
+    await Promise.all(copyPromises);
+  } catch {
+		
+    	throw Error(ERROR_MESSAGE);
+  }
 };
 
 await copy();
