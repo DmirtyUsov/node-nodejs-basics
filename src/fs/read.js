95d92a21def8dad4f1e61a22400df50f3ab5318e
Dima Usov <dmitry.usov@yahoo.com>
2024-10-06 23:20:29 +0400

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = "files";
+const ERROR_MESSAGE = "FS operation failed";
+
+const FILE_NAME = 'fileToRead.txt';
+const FILE_PATH = path.resolve(__dirname, FILES_DIR, FILE_NAME);
+
 const read = async () => {
-    // Write your code here 
+   try {
+     const data = await fs.readFile(FILE_PATH, {
+       encoding: 'utf8',
+     });
+
+     console.log(data);
+   } catch {
+     throw new Error(ERROR_MESSAGE);
+   }
 };
 
 await read();
