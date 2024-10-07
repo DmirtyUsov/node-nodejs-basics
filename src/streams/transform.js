748359dafb420373482bb4f0dca80e925ed1d342
Dima Usov <dmitry.usov@yahoo.com>
2024-10-07 23:34:22 +0400

    feat: add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'node:stream';
+
+const reverseTransform = new Transform({
+  transform(chunk, encoding, callback) {
+    const chars = chunk.toString().split('');
+    const eol = chars.pop();
+    const reversedChars = chars.reverse();
+    reversedChars.push(eol);
+    
+    this.push(reversedChars.join(''));
+
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseTransform).pipe(process.stdout) 
 };
 
 await transform();
