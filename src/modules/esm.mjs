e0dd6aa7f6e45e83900970b0317bcf3814e7ff93
Dima Usov <dmitry.usov@yahoo.com>
2024-10-07 14:10:37 +0400

    feat: add esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,7 +1,14 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import './files/c.js';
+
+import { createRequire } from 'node:module';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 
 const random = Math.random();
 
@@ -33,7 +40,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
