cc4a09f9a0d44547c23a40cc0c58b00cbb91c9b5
Dima Usov <dmitry.usov@yahoo.com>
2024-10-08 01:54:23 +0400

    feat: add Child Processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import path from 'node:path';
+import url from 'node:url';
+import { spawn } from "node:child_process";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILES_DIR = 'files';
+const FILE_NAME = 'script.js';
+const FILES_DIR_PATH = path.resolve(__dirname, FILES_DIR, FILE_NAME);
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [FILES_DIR_PATH, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['one', 2, 'three', 4, 'five']);
